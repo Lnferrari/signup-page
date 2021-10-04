f8bcf3d6be9017ea4aac017563bea4d2a64deb1a
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-10-04 18:48:52 +0200

    setting cloudinary and routes

--- api/server.js
+++ api/server.js
@@ -1,20 +1,21 @@
+import dotenv from 'dotenv'
 import './db-connect.js'
 import express from 'express'
 import cors from 'cors'
 import User from './models/User.js'
-import dotenv from 'dotenv'
 import cloudinary from 'cloudinary'
 
 const app = express();
 dotenv.config();
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 })
 
 /* ----- EXPRESS MIDDLEWARE ----- */
-app.use( express.json({ limit: '1mb'}) )
+app.use( express.json({ limit: '1MB' }) )
 app.use( cors() )
 
 // ENDPOINTS --------------------
@@ -24,11 +25,34 @@ app.get('/', (req, res) => {
 
 
 app.post('/signup', async (req, res, next) => {
-
+  try {
+    const { avatar, ...userData } = req.body
+    const uploadResponse = await cloudinary.v2.uploader.upload(avatar, {
+      overwrite: true,
+      invalidate: true,
+      width: 600, height: 400, crop: "fill"
+    })
+    console.log('uploadResponse =>', uploadResponse)
+    if (uploadResponse.error) {
+      return new Error(`Ups! Something went wrong`)
+    }
+    const newUser = await User.create({
+      ...userData,
+      avatar: uploadResponse.secure_url
+    })
+    res.json( newUser )
+  } catch (err) {
+    next(err)
+  }
 })
 
 app.get('/users', async (req, res, next) => {
-
+  try {
+    const users = await User.find().sort('username')
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
 })
 
 app.use((req, res, next) => {
