e6056646680e9fa7a0c08262b8d57521a1d29667
Lucas Ferrari <ln.ferrari@hotmail.com>
2021-10-07 15:24:40 +0200

    updating

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -5,7 +5,6 @@ import Resizer from "react-image-file-resizer";
 import UserContext from '../context/UserContext';
 
 const initialState = {
-  // avatar: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.sqhUFRHRSP73IW9-wcDMcQHaHa%26pid%3DApi&f=1',
   username: '',
   password: ''
 }
